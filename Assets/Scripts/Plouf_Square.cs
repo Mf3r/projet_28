333849d31c51f861eedd208c15063040dd9ecc3d
Maria <mf.berguerand@etudiant.ecolemelies.fr>
2021-05-21 14:16:44 +0200

    Test

---
+++ Assets/Scripts/Plouf_Square.cs
@@ -0,0 +1,3 @@
+﻿public class Plouf_Square
+{
+}
--- Assets/Scripts/Swim.cs
+++ Assets/Scripts/Swim.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Swim : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    [SerializeField] private float speed;             //float chiffre à virgule
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float jumpForce;
+
+
+    private float direction;                          //flag qui permt de savoir si on move ou ps(v)
+    private bool canJump = false;                    //flag pr savoir si on px sauter(v)
+
+
+    private Controls controls;                  //classe _ (vrt-->obj)
+    private Rigidbody2D rigidbody2D;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+
+    public void OnTriggerEnter2D(Collider2D collision)           //Detection de la zone
     {
-        
+        Debug.Log("Under Water ");
+        canJump = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    
+
+        //useGraviy = false ?
+        //if Ploof_Square is true
+        //rigidbody
+    
+
+
 }
